0c37776f196aa40543b4e2929841e49ead3bdecb
scottsimply <scottistriggered@gmail.com>
2024-05-25 21:19:44 -0500

    Adding usings

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,13 @@
 </script>
 
 <template>
+  <h1 class="title">Connections</h1>
   <game></game>
 </template>
 
 <style scoped>
+  .title {
+    margin-left: auto;
+    margin-right: auto;
+  }
 </style>
