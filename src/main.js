75c305cb25c95b3035b6d45f0ce08153a7cb49e4
scottsimply <scottistriggered@gmail.com>
2024-05-25 21:20:09 -0500

    Adding components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Game from './components/Game.vue'
 import GameButton from './components/GameButton.vue';
+import SolvedLabel from './components/SolvedLabel.vue';
+import Connection from './components/Connection.vue';
 
 createApp(App)
     .component("Game", Game)
     .component("GameButton", GameButton)
+    .component("Connection", Connection)
+    .component("SolvedLabel", SolvedLabel)
     .mount('#app')
