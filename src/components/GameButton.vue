768e5301217a565f9d2f2a9ffee94fbb765be1e3
scottsimply <scottistriggered@gmail.com>
2024-05-25 21:18:52 -0500

    Events go up the component tree

--- src/components/GameButton.vue
+++ src/components/GameButton.vue
@@ -4,12 +4,37 @@
             return {
                 selected: false,
             }
-        }
+        },
+        methods: {
+            on_click() {
+                this.selected = !this.selected;
+                this.$emit('childselected', this.selected, this.$refs.wordSlot.textContent);
+            }
+        },
+        emits: ['childselected']
     }
 </script>
 <template>
-    <button class="game-button"><slot></slot></button>
+    <button class="game-button" v-on:click="on_click" :class="selected ? 'selected' : ''"><span ref="wordSlot"><slot></slot></span></button>
 </template>
 <style>
-
+    * {
+        --button-unselected: #bcf0d2;
+        --button-selected: #35b361;
+    }
+    .game-button {
+        background-color: var(--button-unselected);
+        border: none;
+        margin: 3px 3px 12px 12px;
+        height: 80px;
+        width: 200px;
+    }
+    .selected {
+        background-color: var(--button-selected);
+    }
+    /*    Controls */
+    .game-button:hover {
+        cursor: pointer;
+        filter: brightness(1.15);
+    }
 </style>
