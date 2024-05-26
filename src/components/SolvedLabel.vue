6d4331217d10b7fa350e235b6ecd76884c9e7e8d
scottsimply <scottistriggered@gmail.com>
2024-05-25 21:17:43 -0500

    Adding subcomponents

---
+++ src/components/SolvedLabel.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+    }
+</script>
+<template>
+    <div class="solved-connection">
+        <p class="solved-text"><slot></slot></p>
+    </div>
+</template>
+<style>
+    * {
+        --connection-1: #f6ec91;
+        --connection-2: #ed97c9;
+        --connection-3: #88bffa;
+        --connection-4: #ac8bd6;
+    }
+    .solved-connection {
+        grid-column: 1/-1;
+    }
+</style>
